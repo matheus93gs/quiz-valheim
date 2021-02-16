dd32ef0642005fd52b929509b727aa580561962d
gr8matheus <matheus93gs@hotmail.com>
2021-02-16 17:21:32 -0300

    Att

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
 import styled from 'styled-components';
 import db from'../db.json';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+ 
+const BackgroundImage = styled.div`
+    background-image: url(${db.bg});
+    flex: 1;
+    background-size: cover;
+    object-position: center;
+`;
+
+const Widget = styled.div`
+  
 `
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+`
+
 
 export default function Home() {
   return (
 
-    <div style={{ backgroundImage: `url(${db.bg})` }}>
+    <BackgroundImage>
+      <QuizContainer>
       Teste
-    </div>
+      </QuizContainer>
+    </BackgroundImage>
 
   )
 }
